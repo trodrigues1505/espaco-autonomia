a3b3ac03d1360c174d5246cad42686b713b5c152
trodrigues1505 <74118138+trodrigues1505@users.noreply.github.com>
2026-06-15 10:06:41 -0300

    Update alunos.js

--- src/pages/admin/alunos.js
+++ src/pages/admin/alunos.js
@@ -434,4 +434,4 @@ export async function renderAlunos(container, page) {
     window._pendingEditAluno = null
     setTimeout(() => window.editarAluno && window.editarAluno(idParaEditar), 50)
   }
-}    
+}     
