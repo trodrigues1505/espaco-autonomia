3e26b67a92fc1cd6848a7cb42ba1d1e6f674b893
trodrigues1505 <74118138+trodrigues1505@users.noreply.github.com>
2026-07-13 14:58:14 -0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 /**
  * src/pages/index.js
  */
-const V = '?v=16'
+const V = '?v=17'
 const rotaMap = {
   'dashboard':          () => import('./admin/dashboard.js'  + V).then(m => m.renderDashboard),
   'timeline':           () => import('./timeline.js'         + V).then(m => m.renderTimeline),
